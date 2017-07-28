aed62b0b3ad841f10b83a3973b9d4d89171ccc29
safay <scott.a.fay@gmail.com>
2017-07-27 23:43:38 -0700

    modify led control lib, docs

--- bartclock/led_control_lib.py
+++ bartclock/led_control_lib.py
@@ -3,7 +3,10 @@
 # Modified from Adafruit Dot Star RGB LED strip test code.
 
 import time
+import numpy as np
+
 from dotstar import Adafruit_DotStar
+
 
 
 def init_strip(numpixels, datapin, clockpin):
@@ -47,3 +50,32 @@ def clear_pixel(index):
 
 def light_pixel(index, color, brightness):
     pass
+
+# how would a bart strip look as an object?
+# A strip has state.
+# Lights should brighten and dim in a defined manner; take about 2 seconds for the transition.
+# There should be an update method for the BartStrip
+# When the update is received, A routine moves each pixel to its new value.
+# The actual rate of updates is not significant.  Every 15 sec?  More than often enough.
+# Curious to know how to peg a process to the internal clock.  Look into this.
+# Ramp up to an arbitrary value.
+# 20fps
+# so it would look something like this:
+
+class BartStrip(object): # check to see how to inherit from the DotStar object...
+    # Note: This is not the master controller.  It is the strip controller, and that is all.
+    def __init__(self, numpixels, datapin, clockpin):
+        self.strip = Adafruit_DotStar(numpixels, datapin, clockpin)
+
+    def update(self, bartinfo):
+        # simple implementation: take the bart API info and update the strip
+        pass
+
+    def idle(self):
+        # This is a mode with pretty lights.  For testing and use when BART is not running.
+        modulate = lambda wavelength, height, x: height * (np.sin(x / (wavelength * np.pi)) + 1)
+        # where led_ix is a np array of strip indicies
+        # use this to generate a new array of color intensities.  Overlay R/G/B to cycle through colors
+        array_of_color_intensities = [int(round(x*255)) for x in modulate(0.5, 0.5, led_ix)]
+        pass
+
